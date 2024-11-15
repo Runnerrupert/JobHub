5e10d6436db3ee6805024941af27480d55fed337
Papito <170674313+PeruvPowWow@users.noreply.github.com>
2024-11-14 22:01:28 -0700

    added imports and started adding const for home like customers and employees

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,8 +1,32 @@
 // import { Link } from 'react-router-dom';
 // import { useQuery } from '@apollo/client';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { QUERY_ME } from '../utils/queries';
+import Auth from '../utils/auth';
+import './Home.css';
 
+const { Header, COntent } = Layout;
+const { Title, Text } = Typography;
 
 const Home = () => {
+  const customers = [
+    {
+      name: 'John Doe',
+      address: '123 Main St, USA, US 12345',
+      jobs: [
+        { title: 'Job 1', description: 'Job 1 Description' },
+        { title: 'Job 2', description: 'Job 2 Description' },
+      ],
+    },
+  ];
+
+const employees = [
+  {name: 'John Doe', assignedJob: ['fix plumbing issues', 'install new sink']},
+  {name: 'Jane Doe', assignedJob: ['install new sink', 'fix plumbing issues']},
+];
+      
 
   return (
     <div>
