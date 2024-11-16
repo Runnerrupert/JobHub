f96a73d817eff48149fa509571429e3db0dcdb3f
Cameron Barfuss <Cameron.barfuss@gmail.com>
2024-11-16 15:19:24 -0700

    Add login page functionality

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,8 +1,59 @@
+import { useState, FormEvent, ChangeEvent } from "react";
+import Auth from '../utils/auth';
+import { useMutation } from '@apollo/client';
+import { LOGIN_MANAGER } from '../utils/mutations';
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
-    return(
-        <h1>This is the Login Page</h1>
-    )
-}
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [login, { loading, error }] = useMutation(LOGIN_MANAGER);
+    const navigate = useNavigate();
+  // Checks if the user can be logged in based on information stored within the database
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    try {
+        const { data } = await login({ variables: { email, password } });
+
+        Auth.login(data.login.token);
+        navigate("/")
+    } catch (err) {
+        console.log("Login Failed", err);
+    }
+  };
+    
+  return (
+    <div>
+      <button><Link to='/'>Back</Link></button>
+        <div>
+            <form onSubmit={handleSubmit}>
+              <h2>Login</h2>
+              <div>
+                <label>Email</label>
+                <input
+                    type="email"
+                    value={email}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+                    required
+                />
+              </div>
+              <div>
+                <label>Password</label>
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+                    required
+                />
+              </div>
+              <button type='submit' disabled={loading}> {loading ? "Logging in..." : "Submit"} </button>
+              {error && <p className="error">Error: {error.message}</p>}
+            </form>
+        </div>
+    </div>
+  );
+};
 
 export default Login;
