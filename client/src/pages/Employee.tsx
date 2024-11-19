e14133ca397f278ee24a1f046a48dcb66b9d2ad7
zhill0950 <zhill0950@gmail.com>
2024-11-18 19:15:21 -0800

    Update Employee.tsx

--- client/src/pages/Employee.tsx
+++ client/src/pages/Employee.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import EmployeeForm from '../components/EmployeeForm';
+import EmployeeList from '../components/EmployeeList';
 
 const Employee: React.FC = () => {
   return (
     <div>
       <h1>This is the Employee Page</h1>
       {/* // Add forms to add, update, and delete employees */}
+      <EmployeeForm />
 
       {/* // Add a table to display all employees */}
+      <EmployeeList />
     </div>
   );
 };
