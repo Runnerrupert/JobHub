c8600c5bd0f02c0aa63b0faae53afe6b687043d5
Cameron Barfuss <Cameron.barfuss@gmail.com>
2024-11-19 22:30:24 -0700

    add navbar to each page

--- client/src/pages/Customer.tsx
+++ client/src/pages/Customer.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import CustomerForm from '../components/CustomerForm';
 import CustomerList from '../components/CustomerList';
+import Navbar from '../components/Navbar';
 
 const Customer: React.FC = () => {
   return (
     <div>
-      <h1>This is the Customer Page</h1>
-      {/* // Add forms to add, update, and delete customers */}
+      <Navbar />
+      <div>
+        <h1>This is the Customer Page</h1>
+        {/* // Add forms to add, update, and delete customers */}
 
-      <CustomerForm />
+        <CustomerForm />
 
-      {/* // Add a table to display all customers */}
+        {/* // Add a table to display all customers */}
 
-      <CustomerList />
+        <CustomerList />
+      </div>
     </div>
   );
 };
--- client/src/pages/Jobs.tsx
+++ client/src/pages/Jobs.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 
 const Jobs: React.FC = () => {
     return (
         <div>
-        <h1>This is the Jobs Page</h1>
-        {/* // Add forms to add, update, and delete jobs */}
-        // Add forms to add, update, and delete jobs.
-        {/* // Add a table to display all jobs */}
+            <Navbar/>
+            <div>
+                <h1>This is the Jobs Page</h1>
+                {/* // Add forms to add, update, and delete jobs */}
+                // Add forms to add, update, and delete jobs.
+                {/* // Add a table to display all jobs */}
+            </div>
         </div>
+        
     );
 };
 
