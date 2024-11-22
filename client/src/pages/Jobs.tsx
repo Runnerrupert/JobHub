671d4f8dfbf30a48690a0f354bf498a162216930
Cameron Barfuss <Cameron.barfuss@gmail.com>
2024-11-21 21:16:21 -0700

    modified the assigning to be on the job page

--- client/src/pages/Jobs.tsx
+++ client/src/pages/Jobs.tsx
@@ -2,14 +2,20 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import JobForm from '../components/JobForm';
 import JobList from '../components/JobList';
+import AssignmentForm from '../components/AssignmentForm'
 import type { Job } from '../interfaces/Job';
 
 const Jobs: React.FC = () => {
     const [editingJob, setEditingJob] = React.useState<Job | null> (null);
+    const [assigningJob, setAssigningJob] = React.useState<Job | null> (null);
 
     const handleEditJob = (job: Job) => {
         setEditingJob(job);
     }
+
+    const handleAssignEmployees = (job: Job) => {
+        setAssigningJob(job);
+    };
 
     return (
         <div>
@@ -19,7 +25,9 @@ const Jobs: React.FC = () => {
                 {/* // Add forms to add, update, and delete jobs */}
                 <JobForm job={editingJob} />
                 {/* // Add a table to display all jobs */}
-                <JobList editJob={handleEditJob} />
+                <JobList editJob={handleEditJob} assignJob={handleAssignEmployees} />
+
+                {assigningJob && <AssignmentForm job={assigningJob} />}
             </div>
         </div>
         
