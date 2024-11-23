a02b1cdcc4697c47d28970ffaab273aecc52ff07
Papito <170674313+PeruvPowWow@users.noreply.github.com>
2024-11-23 05:47:17 -0700

    added the component test to the cypress directory. Decided to do a testing on the navbar file component

---
+++ cypress/component/Navbar.cy.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { mount } from '@cypress/react';
+import Navbar from '../../src/components/Navbar';
+
+describe('Navbar Component', () => {
+  it('renders correctly', () => {
+    mount(<Navbar />);
+    cy.get('.navbar').should('exist'); // Adjust selector based on your component.
+    cy.contains('Home').should('exist'); // Replace "Home" with actual text in Navbar.
+  });
+});
