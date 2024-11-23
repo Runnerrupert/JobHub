1aebc37b899483d031399f5615f319c819c1989c
Papito <170674313+PeruvPowWow@users.noreply.github.com>
2024-11-23 07:08:46 -0700

    adding a simpler path to the navbar component so I dont complicate the path to it like it originally was

--- cypress/component/Navbar.cy.tsx
+++ cypress/component/Navbar.cy.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { mount } from '@cypress/react';
-import Navbar from '../../src/components/Navbar';
+import Navbar from '../../client/src/components/navbar';
 
 describe('Navbar Component', () => {
   it('renders correctly', () => {
