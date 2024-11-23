925d81b74ece7c9aeb299d10f3a684d7cd673199
Papito <170674313+PeruvPowWow@users.noreply.github.com>
2024-11-23 08:04:07 -0700

    fixed the import navbar and hopefully everything is working

--- cypress/component/Navbar.cy.tsx
+++ cypress/component/Navbar.cy.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+
 import { mount } from '@cypress/react';
-import Navbar from '../../client/src/components/navbar';
+import Navbar from '@components/Navbar';
 
 describe('Navbar Component', () => {
   it('renders correctly', () => {
